6d1d4eb89e133d6ee5be2bca0979cbb9f4a6c241
Ted Michael Celis <krazete@gmail.com>
2018-02-03 20:48:52 -0800

    is now slightly functional

--- digi/awkn.js
+++ digi/awkn.js
@@ -1,24 +1,67 @@
-var mons = Object.keys(digi).sort();
-mons.forEach(function (mon) {
-    var div = document.createElement("div");
-    div.className = "mon";
-    div.id = mon;
-    var img = document.createElement("img");
-    img.className = "thumb";
-    img.src = "mon/" + mon + ".png";
-    div.appendChild(img);
-    var a = document.createElement("a");
-    a.className = "alt";
-    a.href = "http://growlmon.net/digimon/" + mon;
-    a.target = "_blank";
-    a.innerHTML = mon;
-    div.appendChild(a);
-    document.getElementById(digi[mon].evol).getElementsByClassName("mons")[0].appendChild(div);
-});
+var allDigi = new Set(Object.keys(digi));
+var selectedDigi = new Set();
 
-function evol(mon) {
-    return digi[mon].evol;
+function getBox(id) {
+    return document.getElementById(id).children[1].children[0];
 }
+
+function addTapListener(e, f) {
+    e.addEventListener("click", f);
+    e.addEventListener("touchstart", f);
+}
+
+function selectDigi(mon) {
+    selectedDigi.add(mon);
+    update();
+    return selectedDigi;
+}
+
+function deselectDigi(mon) {
+    selectedDigi.delete(mon);
+    update();
+    return selectedDigi;
+}
+
+function update() {
+    var selection = getBox("selection");
+    selection.innerHTML = "";
+    selectedDigi.forEach(function (mon) {
+        var clone = digi[mon].element.cloneNode(true);
+        clone.className = "mon";
+        addTapListener(clone, function () {
+            deselectDigi(this.id);
+        })
+        selection.appendChild(clone);
+    });
+
+    allDigi.forEach(function (mon) {
+        if (!selectedDigi.has(mon)) {
+            digi[mon].element.classList.add("locked");
+        }
+    });
+}
+
+function getTree(mon) {
+    var tree = new Set();
+    next(mon).forEach(function (nextmon) {
+        tree.add([mon, nextmon]);
+    });
+}
+
+function intersection(t, u) { // TODO
+    var c = new Set();
+    a.forEach(function (mon) {
+        if (b.has(mon)) {
+            c.add(mon);
+        }
+    });
+    return c;
+}
+
+
+
+
+
 
 function next(mon) {
     return digi[mon].dvol;
@@ -34,82 +77,142 @@ function prev(mon) {
     return prevmons;
 }
 
-var input = document.getElementById("in");
-var output = document.getElementById("out");
-input.addEventListener("touchstart", function () {
-    input.focus();
-});
-
-function showmon(mon) {
-    var div = document.createElement("div");
-    div.className = "mon";
-    div.addEventListener("click", function (e) {
-        input.blur();
-        output.innerHTML = "";
-        showmon(mon);
-        var p = document.createElement("p");
-        p.innerHTML = "previous:";
-        output.appendChild(p);
-        prev(mon).forEach(showmon);
-        var p = document.createElement("p");
-        p.innerHTML = "next:";
-        output.appendChild(p);
-        next(mon).forEach(showmon);
-    });
-    div.addEventListener("touchstart", function (e) {
-        input.blur();
-        output.innerHTML = "";
-        showmon(mon);
-        var p = document.createElement("p");
-        p.innerHTML = "previous:";
-        output.appendChild(p);
-        prev(mon).forEach(showmon);
-        var p = document.createElement("p");
-        p.innerHTML = "next:";
-        output.appendChild(p);
-        next(mon).forEach(showmon);
-    });
-    var img = document.createElement("img");
-    img.src = "img/" + mon + ".png";
-    img.alt = mon;
-    div.appendChild(img);
-    output.appendChild(div);
+function line(a, b, color, width) {
+    var path = document.createElement("path");
+    path.setAttribute("d",
+        "M" + a.x + "," + a.y +
+        "S" + a.x + "," + (a.y + 16) +
+        " " + (a.x + b.x) / 2 + "," + (a.y + 16) +
+        "S" + b.x + "," + (a.y + 16) +
+        " " + b.x + "," + b.y
+    );
+    path.setAttribute("stroke", color);
+    path.setAttribute("stroke-width", width);
+    path.setAttribute("fill", "none");
+    var linelayer = document.getElementById("linelayer");
+    linelayer.appendChild(path);
 }
 
-function autocomplete() {
-    output.innerHTML = "";
+function branch(monA, monB, p, k) {
+    var a = digi[monA].element;
+    var b = digi[monB].element;
+    a.classList.add("locked");
+    b.classList.add("locked");
+    var aRect = a.getBoundingClientRect();
+    var bRect = b.getBoundingClientRect();
+    var aMid = {
+        "x": window.scrollX + (aRect.left + aRect.right) / 2,
+        "y": window.scrollY + aRect.bottom - 1
+    };
+    var bMid = {
+        "x": window.scrollX + (bRect.left + bRect.right) / 2,
+        "y": window.scrollY + bRect.top + 1
+    };
+    var linelayer = document.getElementById("linelayer");
+    line(aMid, bMid, p, k);
+    linelayer.innerHTML += ""; // force update
+}
+
+function reset() {
     var mons = Object.keys(digi);
     mons.forEach(function (mon) {
-        if (mon.startsWith(input.value.toLowerCase())) {
-            showmon(mon);
+        digi[mon].element.classList.remove("locked");
+        digi[mon].element.classList.remove("hidden");
+    });
+}
+
+function hide() {
+    var mons = Object.keys(digi);
+    mons.forEach(function (mon) {
+        if (!selection.includes(mon)) {
+            digi[mon].element.classList.add("hidden");
         }
     });
 }
 
-input.addEventListener("input", autocomplete);
-
-function line(a, b) {
-    var aRect = a.getBoundingClientRect();
-    var bRect = b.getBoundingClientRect();
-    var aMid = {
-        "x": (aRect.left + aRect.right) / 2,
-        "y": (aRect.top + aRect.bottom) / 2
-    };
-    var bMid = {
-        "x": (bRect.left + bRect.right) / 2,
-        "y": (bRect.top + bRect.bottom) / 2
-    };
-    var linelayer = document.getElementById("linelayer");
-    var path = document.createElement("path");
-    path.setAttribute("d",
-        "M" + aMid.x + "," + aMid.y +
-        "C" + aMid.x + "," + bMid.y +
-        " " + bMid.x + "," + aMid.y +
-        " " + bMid.x + "," + bMid.y
-    );
-    path.setAttribute("stroke", "black");
-    path.setAttribute("stroke-width", "5");
-    path.setAttribute("fill", "none");
-    linelayer.appendChild(path);
-    linelayer.innerHTML += ""; // force update
+var selection = [];
+var visited = [];
+function tree(mon, direction) {
+    if (direction == 0) {
+        visited = [];
+        var linelayer = document.getElementById("linelayer");
+        linelayer.innerHTML = "";
+    }
+    if (direction < 1) {
+        var prevmons = prev(mon);
+        for (var i = 0; i < prevmons.length; i++) {
+            var prevmon = prevmons[i];
+            var pair = prevmon + "+" + mon;
+            if (!visited.includes(pair)) {
+                tree(prevmon, -1)
+            }
+            visited.push(pair);
+            selection.push(prevmon);
+            selection.push(mon);
+        }
+    }
+    if (direction > -1) {
+        var nextmons = next(mon);
+        for (var i = 0; i < nextmons.length; i++) {
+            var nextmon = nextmons[i];
+            var pair = mon + "+" + nextmon;
+            if (!visited.includes(pair)) {
+                tree(nextmon, 1);
+            }
+            visited.push(pair);
+            selection.push(mon);
+            selection.push(nextmon);
+        }
+    }
+    if (direction == 0) {
+        reset();
+        hide();
+        visited.forEach(function (pair) {
+            var mons = pair.split("+");
+            branch(mons[0], mons[1], "#000", 4);
+        });
+        visited.forEach(function (pair) {
+            var mons = pair.split("+");
+            branch(mons[0], mons[1], "#fff", 2);
+        });
+    }
 }
+
+
+function init() {
+    var any = document.getElementById("any");
+    var all = document.getElementById("all");
+    addTapListener(any, function (e) {
+        any.classList.add("selected");
+        all.classList.remove("selected");
+    });
+    addTapListener(all, function (e) {
+        any.classList.remove("selected");
+        all.classList.add("selected");
+    });
+    any.click();
+
+    var mons = Object.keys(digi).sort(); // sort is redundant, but just in case
+    mons.forEach(function (mon) {
+        var div = document.createElement("div");
+        div.className = "mon";
+        div.id = mon;
+            var img = document.createElement("img");
+            img.className = "thumb";
+            img.src = "mon/" + mon + ".png";
+        div.appendChild(img);
+            var a = document.createElement("a");
+            a.className = "alt";
+            a.href = "http://growlmon.net/digimon/" + mon;
+            a.target = "_blank";
+            a.innerHTML = mon;
+        div.appendChild(a);
+        addTapListener(div, function (mouse) {
+            selectDigi(this.id);
+        });
+        digi[mon].element = div;
+        getBox(digi[mon].evol).appendChild(div);
+    });
+}
+
+init();
