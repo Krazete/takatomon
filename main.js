0d2f7797a59ed1672e9fa2e11b46995a05b9f511
Ted Michael Celis <krazete@gmail.com>
2018-03-03 14:29:41 -0800

    idk

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-.use(express.static(path.join(__dirname, 'public')))
+.use(express.static(path.join(__dirname, '')))
+
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
