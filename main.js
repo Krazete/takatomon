a767ae7a6158fd247c8dce9b3eab4b717d18a3ae
Ted Michael Celis <krazete@gmail.com>
2018-06-18 20:53:45 -0700

    removed heroku stuff

--- main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-.use(express.static(path.join(__dirname, '')))
-
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
