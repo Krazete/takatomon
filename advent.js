92237fca02c2baf0ce6b4485e5f036b7962d365f
Ted Michael Celis <krazete@gmail.com>
2018-06-24 02:30:05 -0700

    added advent schedule for future update

---
+++ advent.js
@@ -0,0 +1,15 @@
+var advent = {
+    /* advents */
+    "groundlocomon": [new Date("06-11-2018 02:00:00 PDT"), new Date("06-25-2018 01:59:59 PDT")],
+    "puppetmon":     [new Date("06-11-2018 02:00:00 PDT"), new Date("06-25-2018 01:59:59 PDT")],
+    "kuzuhamon":     [new Date("06-18-2018 02:00:00 PDT"), new Date("07-02-2018 01:59:59 PDT")],
+    "princemamemon": [new Date("06-18-2018 02:00:00 PDT"), new Date("07-02-2018 01:59:59 PDT")],
+    "saberleomon":   [new Date("06-25-2018 02:00:00 PDT"), new Date("07-09-2018 01:59:59 PDT")],
+    "megagargomon":  [new Date("06-25-2018 02:00:00 PDT"), new Date("07-09-2018 01:59:59 PDT")],
+    "justimon":      [new Date("07-02-2018 02:00:00 PDT"), new Date("07-16-2018 01:59:59 PDT")],
+    "minervamon":    [new Date("07-02-2018 02:00:00 PDT"), new Date("07-16-2018 01:59:59 PDT")],
+    "ebemon":        [new Date("07-09-2018 02:00:00 PDT"), new Date("07-23-2018 01:59:59 PDT")],
+    "titamon":       [new Date("07-09-2018 02:00:00 PDT"), new Date("07-23-2018 01:59:59 PDT")],
+    /* events */
+    "gallantmon":    [new Date(), new Date()]
+};
