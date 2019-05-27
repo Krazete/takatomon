73ff70a9a49e7933b199d4bdd90ffb001ec6d00b
Ted Michael Celis <krazete@gmail.com>
2019-05-27 11:07:18 -0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,57 +1,24 @@
 const CACHE = "pwabuilder-offline";
 
-const offlineFallbackPage = "index.html";
-
-// Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
-
-  event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Cached offline page during install");
-      return cache.add(offlineFallbackPage);
-    })
-  );
+self.addEventListener("install", function (e) {
+    e.waitUntil(caches.open(CACHE).then(function (cache) {
+        return cache.add("/"); /* cache index page*/
+    }));
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(function (response) {
-        console.log("[PWA Builder] add page to offline cache: " + response.url);
-
-        // If request was success, add or update it in the cache
-        event.waitUntil(updateCache(event.request, response.clone()));
-
-        return response;
-      })
-      .catch(function (error) {
-        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
-        return fromCache(event.request);
-      })
-  );
+self.addEventListener("fetch", function (e) {
+    if (e.request.method == "GET") {
+        e.respondWith(fetch(e.request).then(function (response) {
+            e.waitUntil(updateCache(e.request, response.clone())); /* cache requested page */
+            return response;
+        }).catch(function (error) { /* if network fails */
+            return cache.match(e.request);
+        }));
+    }
 });
-
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return error page
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
-      }
-
-      return matching;
-    });
-  });
-}
 
 function updateCache(request, response) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
+    return caches.open(CACHE).then(function (cache) {
+        return cache.put(request, response);
+    });
 }
